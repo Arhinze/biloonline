87caeb0fc886f49f437a19fe716d88a7d66a36ac
Arinze <francisokoye48@gmail.com>
2025-06-22 03:27:32 +0100

    creating admin page

--- orders.php
+++ orders.php
@@ -21,7 +21,7 @@ if(isset($_COOKIE["admin_name"]) && isset($_COOKIE["admin_password"])){
             <div id="filter_options" style="display:block;padding:9px;background-color:#f3f3f3;border-radius:6px;border:1px dotted #000;position:fixed;top:30%;width:80%;line-height:24px">
                 <div><a href="/orders?filter=processing" style="color:#f3d111">Processing</a></div>
                 <div><a href="/orders?filter=delivered" style="color:green">Delivered</a></div>
-                <div><a href="/orders?filter=cancelled" style="color:red">Canceled</a></div>
+                <div><a href="/orders?filter=cancelled" style="color:red">Cancelled</a></div>
                 <div><a href="/orders" style="color:#000">-- View All --</a></div>
                 <div class="edit_product_action_button" style="background-color:#ff9100;margin-top:15px;width:fit-content" onclick="show_div('filter_options')"><i class="fa fa-ban"></i>&nbsp; Close</div>
             </div>
