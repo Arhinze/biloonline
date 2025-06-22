7456a3281bf05814113fde60e120868e77cc8cf6
Arinze <francisokoye48@gmail.com>
2025-06-22 01:29:14 +0100

    creating admin page

---
+++ orders.php
@@ -0,0 +1,275 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"]."/views/admin_Segments.php");
+
+if(isset($_COOKIE["admin_name"]) && isset($_COOKIE["admin_password"])){
+    $stmt = $pdo->prepare("SELECT * FROM `admin` WHERE admin_name = ? AND admin_password = ?");
+    $stmt->execute([$_COOKIE["admin_name"], $_COOKIE["admin_password"]]);
+
+    $data = $stmt->fetch(PDO::FETCH_OBJ);
+    if($data){
+        //that means admin is logged in
+        admin_Segments::header();
+?>
+        <div class="dashboard_div" style="margin:-30px 3% 10% 3%;">
+
+        <h1 style="margin:12px 6px">orders - <?=$site_name?></h2>
+
+        <!-- Add new product div starts -->
+        <div>
+            <div onclick="show_div('filter_options')" style="background-color:green;color:#fff;font-weight:bold;padding:9px 12px;border-radius:6px;margin:12px 0 18px 0;width:fit-content"><span>Filter</span> <i class="fa fa-angle-down" style="margin-left:12px;font-size:21px"></i></div>
+
+            <div id="filter_options" style="display:none;padding:9px;background-color:#f3f3f3;border-radius:6px;border:1px dotted #000">
+                <div>Processing</div>
+                <div>Delivered</div>
+                <div>Canceled</div>
+                <span class="edit_product_action_button" style="background-color:#ff9100" onclick="show_div('filter_options')"><i class="fa fa-ban"></i>Close</span>
+            </div>
+        </div>
+        <!-- Add new product div ends -->
+<?php
+        //check if admin is searching for someone:
+?>
+        <div style="margin-top:18px;padding:12px 9px;border:1px solid #000;border-radius:12px;background-color:#f3f3f3">
+            <input type="text" onkeyup="ajax_search()" id="search_input" class="input" placeholder="Enter Product Name: try: abc" style="border:1px solid #000;width:75%"/>
+        
+            <i class="fa fa-search" onclick ="search_icon()" style="padding:9px;border-radius:4px;font-size:16px;color:#fff;background-color:#000"></i>
+
+            <div id="search" style="position:absolute;width:75%"></div>
+        </div>
+        
+        <div style="margin-top:12px">    <!-- 'main' div starts -->
+            <div class="table_row_div" style="margin-bottom:18px">
+                <div class="table_row" style="width:8%">#</div>
+                <div class="table_row">Product ID</div>
+                <div class="table_row">Status</div>
+                <div class="table_row">View</div>
+            </div>
+<?php
+        //To Edit Product:
+        if(isset($_POST["order_id"])){
+            //check if product still exists
+            $edit_stmt = $pdo->prepare("SELECT * FROM orders WHERE order_id = ?");
+            $edit_stmt->execute([$_POST["order_id"]]);
+    
+            $edit_data = $edit_stmt->fetch(PDO::FETCH_OBJ);
+            if($edit_data){ 
+                //then edit:
+                $edd_stmt = $pdo->prepare("UPDATE orders SET order_name = ?, product_url = ?, `description` = ? WHERE order_id = ?");
+
+                $edd_stmt->execute([$_POST["order_name"], $_POST["url"], $_POST["product_description"], $_POST["order_id"]]);
+
+                echo "<h4 style='color:green'>Product: ", $edit_data->order_name, " has been Updated successfully</h4>";
+            }
+        }
+
+        //To Delete Product:
+        if(isset($_POST["remove_product"])){
+            //check if product still exists
+            $ds_stmt = $pdo->prepare("SELECT * FROM orders WHERE order_id = ?");
+            $ds_stmt->execute([$_POST["remove_product"]]);
+    
+            $ds_data = $ds_stmt->fetch(PDO::FETCH_OBJ);
+            if($ds_data){ 
+                //then delete
+                $dd_stmt = $pdo->prepare("DELETE FROM orders WHERE order_id = ?");
+                $dd_stmt->execute([$_POST["remove_product"]]);
+
+                echo "<h4 style='color:red'>Product: ", $ds_data->order_name, " has been deleted successfully</h4>";
+            } else {
+                echo "<h4 style='color:red'>Error: Product not found.</h4>";
+            }
+        }
+
+        //Mail Customer:
+
+
+        //Select and view all users for easy decision making:
+
+        //A Simple Pagination Algorithm:
+        $p = 1;
+        $num_of_rows = 10;
+
+        if(isset($_GET["page"])){
+            $p = htmlentities($_GET["page"]);
+            if(!is_numeric($p) || $p < 1){
+                $p = 1;
+            }
+        }
+        
+        $page_to_call = ($p - 1)*$num_of_rows;
+
+        //count entire users:
+        $u_search_stmt = $pdo->prepare("SELECT * FROM orders ORDER BY order_id DESC LIMIT ?, ?");
+        $u_search_stmt->execute([0, 1000]);
+
+        $num_of_users = count($u_search_stmt->fetchAll(PDO::FETCH_OBJ));
+
+        $max = ceil($num_of_users/$num_of_rows);
+        // -- end of pagination algorithm --
+
+        
+        //first check if admin searched for a product in particular
+        if(isset($_GET["product"])){
+            $search_q = htmlentities($_GET["product"]);
+
+            $u_search_stmt = $pdo->prepare("SELECT * FROM orders WHERE order_name LIKE ? ORDER BY order_id DESC LIMIT ?, ?");
+            $u_search_stmt->execute(["%$search_q%",$page_to_call, $num_of_rows]);
+
+            $u_data = $u_search_stmt->fetchAll(PDO::FETCH_OBJ);
+        }  else {
+            //if no particular person is searched for, call out everyone:
+            $u_stmt = $pdo->prepare("SELECT * FROM orders ORDER BY order_id DESC LIMIT ?, ?");
+            $u_stmt->execute([$page_to_call, $num_of_rows]);
+    
+            $u_data = $u_stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        if(count($u_data)>0){     
+            $i = 0;
+            foreach($u_data as $d){
+                $i += 1;
+?>
+        <div class="everything-both-buttons-nd-hidden-divs"> 
+                <div class="table_row_div">
+                    <div class="table_row" style="width:8%"><?=$i + (($p - 1)*$num_of_rows)?>. </div>
+                    
+                    <div class="table_row"><b><a href="/product/<?=$d->product_url?>"><?=$d->order_name?> </a></b></div>
+                    
+                    <div class="table_row" style="font-size:fit-content">
+                        <button onclick = "create_content('edit',<?=$i?>)" style="background-color:green"
+                    class="table_row_ED">
+                            Edit &nbsp; <i class="fa fa-pencil"></i> 
+                        </buton>
+
+                        <button onclick = "create_content('remove',<?=$i?>)" style="background-color:red" class="table_row_ED">
+                            <i class="fa fa-warning"></i> &nbsp; Remove </buton>
+                    </div>
+                </div>
+
+
+            <div class="clear">
+            <div style="margin-top:12px;">
+
+            <!-- To make all hidden div content to appear in the same spot on display: -->
+            <div id="content_space<?=$i?>" class="calculator" style="display:none"> 
+                <!-- style="display:block creates undesirable problems like making the div not to appear even onclick" -->
+            </div>
+
+            <!--hidden section 1: Order Details -->
+            <div id="edit<?=$i?>" style="display:none;border:2px solid #888;border-radius:6px;margin-top:12px;padding:9px;">
+
+                <form method="post" action="">
+                    <!-- -->
+                    <div>
+                        <p><b>Order ID:</b> <?=$d->order_id?></p>
+                        <p><b>Order Title:</b> <?=$d->order_name?></p>
+                    </div>
+
+                    <div class="additional_product_images_div_container">
+                        <div class="additional_product_images_div">
+                            <img src = "/static/images/<?=$d->image1?>" class="additional_product_image"/>
+                        </div>
+                        <div class="additional_product_images_div">
+                            <img class="additional_product_image"/>
+                        </div>
+                        <div class="additional_product_images_div">
+                            <img class="additional_product_image"/>
+                        </div>
+                    </div>
+
+                    <div>
+                        <p style="font-size:21px"><b>Contact Details</b> </p>
+                        <p><b>Name:</b> </p>
+                        <p><b>Phone Number:</b> </p>
+                        <p><b>Email:</b> </p>
+                    </div>
+
+                    <div>
+                        Status: 
+                        <select name="order_status">
+                            <option value="processing">processing</option>
+                            <option value="delivered">delivered</option>
+                            <option value="cancelled">cancelled</option>
+                        </select>
+                    </div>
+
+                    <div style="margin:12px 0">
+                        <input type="submit" value="Update Status" class="edit_product_action_button" style="background-color:green"/>
+                        
+                        <span class="edit_product_action_button" style="background-color:#ff9100" onclick="hide_content_space('edit',<?=$i?>)">Cancel</span>
+                    </div>
+                    <input type="hidden" name="order_id" value="<?=$d->order_id?>"/>
+                </form>
+            </div>
+            <!--End of Order Details div-->
+
+
+
+            <!-- hidden section 2: Remove Product -->
+            <!--
+            <div id="remove<=$i?>" style="display:none;border:2px solid red;border-radius:6px;margin-top:12px;padding:3px">
+
+            <form method="post" action="" id="message_form<=$i?>" class="pop_up">
+            <span style="text-align:center">Are you sure you want to remove user: <b style="font-size:18px;color:red;border-bottom:2px solid #fff"><=$d->order_name?>?</b> &nbsp;
+
+            <b>This can't be Undone</b></span><br /><br />
+
+            <input type="hidden"  name="remove_product" value="<=$d->order_id?>"/>
+
+            <input type="submit" value="Remove" style="background-color:red;padding:3px;margin:3px;border-radius:6px;color:#fff;border:none;height:24px;"/> 
+            <span onclick="hide_content_space('remove',<=$i?>)" style="background-color:#ff9100;
+                    padding:3px;border-radius:6px;color:#fff;
+                    margin-left:6px;text-align:center;height:24px;border:none">
+                    Cancel 
+            </span>    
+            </form>
+    
+            </div> -->
+            <!--End of remove<=i> div-->
+
+            </div><!--End of hidden divs-->
+            </div><!--End of hidden divs clear class-->   
+        </div> <hr /><br /><!--End of Both Buttons and hidden divs-->
+        
+        <!--End of all - both buttons hidden divs-->
+
+<?php
+            }
+        }
+?>
+
+        <!--Paginator-->
+        <div class="clear" style="font-weight:bold;font-size:18px; margin-bottom:12px">
+            <?php if($p > 1) { ?> 
+                <div style="float:left">
+                    <b>
+                       <a href="?page=<?=$p-1?>" style="color:#000;font-weight:bold"><i class="fa fa-angle-left"></i> &nbsp; Previous</a>
+                    </b>
+                </div> 
+            <?php } ?>
+
+            <?php if($p < $max) { ?> 
+                <div style="float:right">
+                    <b>
+                        <a href="?page=<?=$p+1?>" style="color:#000">Next &nbsp;<i class="fa fa-angle-right"></i></a>
+                    </b>
+                </div> 
+            <?php } ?>
+        </div> <!-- End of Paginator -->
+
+        </div> <!-- End of class 'main_div' -->
+
+<?php
+    }else{
+        //redirect
+        header("location:/admin");
+    }
+
+    echo "</div>"; //end of 'main' div
+    admin_Segments::footer();
+} else {
+    //redirect
+    header("location:/admin");
+}
+
+?>
