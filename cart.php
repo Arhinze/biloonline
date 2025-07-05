a0be0a2e65f6e95593400dd458ce8b18ee3fbdd6
Arinze <francisokoye48@gmail.com>
2025-07-05 22:23:14 +0100

    creating cart page

--- cart.php
+++ cart.php
@@ -3,12 +3,25 @@
 include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
 
 $customer_id = 1;
+$total_amount = 0;
 $cart_stmt = $pdo->prepare("SELECT * FROM orders_processor WHERE customer_id =  ? AND qty > ? LIMIT ?, ?");
 $cart_stmt->execute([$customer_id, 0, 0, 25]);
 $cart_data = $cart_stmt->fetchAll(PDO::FETCH_OBJ);
 $cart_count = count($cart_data);
+
+if($cart_count > 0) {
+    foreach($cart_data as $cc){
+        $p_stmt = $pdo->prepare("SELECT * FROM products WHERE product_id = ?");
+        $p_stmt->execute([$cc->product_id]);
+        $p_data = $p_stmt->fetch(PDO::FETCH_OBJ);
+
+        $total_amount += $p_data->price*$cc->qty;
+    }
+}
 Index_Segments::header(); 
 ?>
+
+
 <div class="main_body" style="margin:0"><!-- .main_body starts -->
 
 <?php
@@ -30,9 +43,9 @@ if (count($cart_data) > 0) {//that means user has an item or more in cart -- lis
                 <div style="font-size:12px"><a href="/product/<?=$cpd->product_url?>" style="color:#888"><?=$short_description?></a></div>
                 <div style="margin:9px 0">
                     <span class="qty">
-                        <b style="font-size:24px" onclick='ajax_qty("$product_id","decrease")'>-</b>&nbsp;&nbsp;
-                        <span id="qty"><?=$cart_d->qty?></span>&nbsp;&nbsp;
-                        <b style="font-size:18px" onclick='ajax_qty("$product_id","increase")'>+</b>
+                        <b style="font-size:24px" onclick='ajax_qty("<?=$cart_d->product_id?>","decrease","qty<?=$cart_d->orders_processor_id?>")'>-</b>&nbsp;&nbsp;
+                        <span id="qty<?=$cart_d->orders_processor_id?>"><?=$cart_d->qty?></span>&nbsp;&nbsp;
+                        <b style="font-size:18px" onclick='ajax_qty("<?=$cart_d->product_id?>","increase","qty<?=$cart_d->orders_processor_id?>")'>+</b>
                     </span>
                 </div>
                 <div style="margin-top:16px">
@@ -48,14 +61,54 @@ if (count($cart_data) > 0) {//that means user has an item or more in cart -- lis
 }
 ?>
 
+    <div class="add_to_my_picks" style="display:flex"><!-- .add_to_my_picks starts -->
+        <div style="margin:9px 15px 0 21%;font-weight:bold">
+            NG N<span id="total_amount"><?=number_format($total_amount)?></span> &nbsp; <i class="fa fa-angle-up"></i>
+        </div>
+        <div class="long_action_button" style="background-color:#ff9100;box-shadow: 0 0 6px #888 inset;width:fit-content;padding:9px 18px">
+            <b>Checkout</b> &nbsp; <i class="fa fa-chevron-circle-right"></i>
+        </div>
+    </div><!-- .add_to_my_picks ends -->
 </div><!-- .main_body ends -->
+
+<script>
+    function ajax_qty(prod_id, incr_or_decr, qty_id) {
+        let obj = new XMLHttpRequest;
+        obj.onreadystatechange = function(){
+            if(obj.readyState == 4){
+                if (document.getElementById(qty_id)){
+                    document.getElementById(qty_id).innerHTML = obj.responseText;
+                }
+            }
+            //update the total amount:
+            ajax_get_total_amount();
+        }
+ 
+        obj.open("GET","/ajax/ajax_qty.php?id="+prod_id+"&increase_or_decrease="+incr_or_decr);
+        obj.send(null);    
+    }
+
+    function ajax_get_total_amount() {
+        let obj = new XMLHttpRequest;
+        obj.onreadystatechange = function(){
+            if(obj.readyState == 4){
+                if (document.getElementById("total_amount")){
+                    document.getElementById("total_amount").innerHTML = obj.responseText;
+                }
+            }
+        }
+ 
+        obj.open("GET","/ajax/ajax_total_amount.php");
+        obj.send(null);    
+    }
+</script>
 
 <?php
 
 Index_Segments::footer($site_name = SITE_NAME_SHORT, $site_url = SITE_URL, $additional_scripts = Index_Segments::index_scripts(),$whatsapp_chat = "off");
         echo <<<HTML
-            <!-- this div is only meant to bring up the footer section of product page so that it's not covered by the fixed 'add_to_my_picks' div-->
-            <!--<div style="margin-top:45px"></div>-->
+            <!-- this div is only meant to bring up the footer section of cart page so that it's not covered by the fixed 'add_to_my_picks' div-->
+            <div style="margin-top:45px"></div>
 HTML;
 
 ?>
