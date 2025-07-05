94c0b858a557e2af2a8ce5be5f86427865854893
Arinze <francisokoye48@gmail.com>
2025-07-05 09:58:11 +0100

    completing product page

--- cart.php
+++ cart.php
@@ -27,7 +27,7 @@ if (count($cart_data) > 0) {//that means user has an item or more in cart -- lis
                 <img src="/static/images/<?=$cpd->image1?>" style="width:100%;height:auto;overflow:hidden;border-radius:9px"/>
             </div>
             <div>
-                <div style="font-size:12px;color:#888"><?=$short_description?></div>
+                <div style="font-size:12px"><a href="/product/<?=$cpd->product_url?>" style="color:#888"><?=$short_description?></a></div>
                 <div style="margin:9px 0">
                     <span class="qty">
                         <b style="font-size:24px" onclick='ajax_qty("$product_id","decrease")'>-</b>&nbsp;&nbsp;
