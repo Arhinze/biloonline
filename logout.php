65c4c505a46904e497286ae5bd299841b3225864
Arinze <francisokoye48@gmail.com>
2025-07-12 16:26:05 +0100

    fixing one or two things in main page

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 
 include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
 
-$cookies = ["username_or_email","password","admin_name","admin_password"];
+$cookies = ["username_or_email","password","admin_name","admin_password", "unique_id"];
 foreach($cookies as $c){
     if(isset($_COOKIE["$c"])){
         setcookie($c, $_COOKIE["$c"], time()-(24*3600), "/");
@@ -15,7 +15,7 @@ Index_Segments::header();
 <div class="dashboard_div" style="text-align:center;padding-bottom:21px">   
     <h3>You've Logged Out Successfully</h3>
 
-    <b><a href="/admin">Login Again</a> or Proceed to <a href="/"><i class="fa fa-home"></i> Home</a></b>
+    <b><a href="/login">Login Again</a> or Proceed to <a href="/"><i class="fa fa-home"></i> Home</a></b>
 </div>
 
 <?php
