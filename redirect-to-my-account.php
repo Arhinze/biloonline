d122638d726d4a3ee605f8d8b9111c46e8f9cc06
Arinze <francisokoye48@gmail.com>
2025-07-13 13:11:51 +0100

    creating sign up and login pages

--- redirect-to-my-account.php
+++ redirect-to-my-account.php
@@ -4,7 +4,7 @@ Index_Segments::header();
 ?>
 
 <div class="main_body">
-    <div style="margin:21px 9px"><a class="input" href="/my-account">Continue to my Account <i class="fa fa-arrow-right"></i></a></div>
+    <div style="margin:21px 9px"><a class="input" style="color:#fff;background-color:#ff9100;font-weight:bold" href="/my-account">Continue to my Account &nbsp; <i class="fa fa-arrow-right"></i></a></div>
 </div>
 
 <?php Index_Segments::footer(); ?>
