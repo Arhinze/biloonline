7331df28147948bbae2d4a82d79ae8dca757c882
Arinze <francisokoye48@gmail.com>
2025-07-13 13:09:34 +0100

    creating sign up and login pages

--- redirect-to-my-account.php
+++ redirect-to-my-account.php
@@ -1,5 +1,10 @@
 <?php
-
-header("Location: /my-account");
-
+include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
+Index_Segments::header();
 ?>
+
+<div class="main_body">
+    <div style="margin:21px 9px"><a class="input" href="/my-account">Continue to my Account <i class="fa fa-arrow-right"></i></a></div>
+</div>
+
+<?php Index_Segments::footer(); ?>
