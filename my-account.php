c2e2a640bccb59c0a529aa2ad1cc95adb86a1fdf
Arinze <francisokoye48@gmail.com>
2025-07-13 13:27:29 +0100

    creating sign up and login pages

--- my-account.php
+++ my-account.php
@@ -4,11 +4,6 @@ include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
 
 Index_Segments::header(); 
 
-$user_unique_id = htmlentities($_COOKIE["unique_id"]);
-$stmt = $pdo->prepare("SELECT * FROM customers WHERE unique_id = ? LIMIT ?, ?");
-$stmt->execute([$user_unique_id, 0, 1]);
-$data = $stmt->fetch(PDO::FETCH_OBJ);
-
 ?>
 
 <div class="main_body" style="margin:0"><!-- .main_body starts -->
@@ -18,7 +13,6 @@ $data = $stmt->fetch(PDO::FETCH_OBJ);
 ?>
         <h1>Hi <?=$data->customer_realname?>, Welcome to BiloOnline</h1>
         <h4>Your email address is: <?=$data->customer_email?></h4>
-        <div style="width:120px;height:120px;border-radius:100%"><img src="/images/customer_image??" style="width:100%;height:auto"/></div>
 <?php
     } else {//if user is not logged in:
 ?>
