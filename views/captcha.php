c4b2367f4c993c7b438ca30f6e612867d000debb
Arinze <francisokoye48@gmail.com>
2025-07-13 14:20:53 +0100

    creating sign up and login pages

--- views/captcha.php
+++ views/captcha.php
@@ -18,5 +18,5 @@
     <input type="hidden" name="xsrf_code" value="<?=$code?>"/>
     
     <!-- Visible Input Tag for user:-->
-    <div style="margin-top:8px;width:50%"><input type = "number" placeholder = "The 6 Digit Code: example - 123456" name = "user_code" class="input" style="height:30px;width:90%"/></div>
+    <div style="margin-top:8px;width:50%"><input type = "number" placeholder = "The 6 Digit Code: example - 123456" name = "user_code" class="input" style="height:30px;width:81%"/></div>
 </div>
