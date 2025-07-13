cd003bbcdc406fefae6b9238faa0989f131938c6
Arinze <francisokoye48@gmail.com>
2025-07-13 23:45:16 +0100

    creating sign up and login pages

--- create-or-login.php
+++ create-or-login.php
@@ -31,8 +31,7 @@ if(isset($_POST["user_code"]) && (!empty($_POST["user_code"])) && (htmlentities(
     } else {//user has no account ~ display create account form:
         $heading = "Create Account";
         $full_name_tag = '<div style="margin-top:9px"><input name = "full_name" type="text" class="input" placeholder="Enter Full Name:"/></div>';
-        $repeat_password_tag = '<div><input type="text" name="repeat_password" class="input" id="password2"  '.'onkeyup='."check_password('password1','password2') ".'placeholder="Repeat Password: ******"/></div><div id="status"></div>';
-        
+        $repeat_password_tag = '<div><input type="text" name="repeat_password" class="input" id="password2"  '.'onkeyup='."check_password('password1','password2') ".'placeholder="Repeat Password: ******" required/></div><div id="status"></div>';      
     }
 
 ?>
@@ -53,11 +52,11 @@ if(isset($_POST["user_code"]) && (!empty($_POST["user_code"])) && (htmlentities(
                     if ($google_or_email_log_in == "") { //that means user has not logged in with google account before or has added a password to his account
                     ?>
                         <h3>Password:</h3>
-                        <div style="margin:6px 0"><input type="text" name="password" class="input" id="password1" placeholder="Enter Password: ******"/></div>
+                        <div style="margin:6px 0"><input type="text" name="password" class="input" id="password1" placeholder="Enter Password: ******" required/></div>
 
                         <?=$repeat_password_tag?>
 
-                        <div style="margin:9px 0 24px 0;width:100%"><button class="input" style="padding:9px 36%;border-radius:30px;color:#fff;font-weight:bold;background-color:#ff9100">Continute</button></div>
+                        <div style="margin:9px 0 24px 0;width:100%" id="continue_button"><button class="input" style="padding:9px 36%;border-radius:30px;color:#fff;font-weight:bold;background-color:#ff9100">Continute</button></div>
                     <?php
                     } else {//user has previously logged in with google, suggest him to do same again or add(reset) password
                         echo $google_or_email_log_in;
@@ -88,6 +87,8 @@ if(isset($_POST["user_code"]) && (!empty($_POST["user_code"])) && (htmlentities(
             document.getElementById("status").innerHTML = "<b style='color:green'>Nice. Passwords Match <i class='fa fa-check'></i></b>";
         } else {
             document.getElementById("status").innerHTML = "<p style='color:red'><b>Passwords Do Not Match</b>.</p> <small><i class='fa fa-warning' style='color:red'></i> Make sure both password fields match to avoid starting the entire process afresh.<br /></small>";
+
+            document.getElementById("continue_button").innerHtml = '<span class="input" style="padding:9px 36%;border-radius:30px;color:#fff;font-weight:bold;background-color:#888">Continute</span>';
         }
     }
 </script>
