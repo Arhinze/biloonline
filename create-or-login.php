71dd4c244db3cd08bf8bf7be903eabec719faff1
Arinze <francisokoye48@gmail.com>
2025-07-13 23:55:45 +0100

    creating sign up and login pages

--- create-or-login.php
+++ create-or-login.php
@@ -30,7 +30,7 @@ if(isset($_POST["user_code"]) && (!empty($_POST["user_code"])) && (htmlentities(
         }
     } else {//user has no account ~ display create account form:
         $heading = "Create Account";
-        $full_name_tag = '<div style="margin-top:9px"><input name = "full_name" type="text" class="input" placeholder="Enter Full Name:"/></div>';
+        $full_name_tag = '<div style="margin-top:9px"><input name = "full_name" type="text" class="input" placeholder="Enter Full Name:" required/></div>';
         $repeat_password_tag = '<div><input type="text" name="repeat_password" class="input" id="password2"  '.'onkeyup='."check_password('password1','password2') ".'placeholder="Repeat Password: ******" required/></div><div id="status"></div>';      
     }
 
