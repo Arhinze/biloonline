4c74aa690af96386abd189b98b9bb5921c187738
Arinze <francisokoye48@gmail.com>
2025-07-15 17:10:07 +0100

    adding option to remove item from cart in cart page

--- ajax/ajax_add_to_num_of_products.php
+++ ajax/ajax_add_to_num_of_products.php
@@ -12,14 +12,14 @@ if(isset($_GET["id"])) {
 $first_sel_stmt = $pdo->prepare("SELECT * FROM orders_processor WHERE customer_id = ? AND product_id = ?");
 $first_sel_stmt->execute([$customer_id, $product_id]);
 $first_sel_data = $first_sel_stmt->fetchAll(PDO::FETCH_OBJ);
-if (count($first_sel_data) == 0) {//that means order is not yet recorded, --record it now:
-    $insert_stmt = $pdo->prepare("INSERT INTO orders_processor(customer_id, product_id, qty) VALUES(?,?,?)");
-    $insert_stmt->execute([$customer_id,$product_id,1]);
+//if (count($first_sel_data) == 0) {//that means order is not yet recorded, --record it now:
+//    $insert_stmt = $pdo->prepare("INSERT INTO orders_processor(customer_id, product_id, qty) VALUES(?,?,?)");
+//    $insert_stmt->execute([$customer_id,$product_id,1]);
 
-    //$select_processing_orders_stmt = $pdo->prepare("SELECT * FROM orders_processor WHERE customer_id = ? AND qty > ?");
-    //$select_processing_orders_stmt->execute([$customer_id, 0]);
-    //$select_processing_orders_data = $select_processing_orders_stmt->fetchAll(PDO::FETCH_OBJ);
-} 
+    ////$select_processing_orders_stmt = $pdo->prepare("SELECT * FROM orders_processor WHERE customer_id = ? AND qty > ?");
+    ////$select_processing_orders_stmt->execute([$customer_id, 0]);
+    ////$select_processing_orders_data = $select_processing_orders_stmt->fetchAll(PDO::FETCH_OBJ);
+//} 
 
 $select_processing_orders_stmt = $pdo->prepare("SELECT * FROM orders_processor WHERE customer_id = ? AND qty > ?");
 $select_processing_orders_stmt->execute([$customer_id, 0]);
