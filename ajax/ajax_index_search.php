2335860617bf4e2a651ceaf2bf265dab05a08d33
Arinze <francisokoye48@gmail.com>
2025-07-19 06:53:28 +0100

    making index search bar to start working

--- ajax/ajax_index_search.php
+++ ajax/ajax_index_search.php
@@ -5,19 +5,20 @@ include_once($_SERVER["DOCUMENT_ROOT"]."/php/connection.php");
 if(isset($_GET["search_query"])){
     if(!empty($_GET["search_query"])){
         $search_q = htmlentities($_GET["search_query"]);
-        $page = htmlentities($_GET["page"]);
        
-    $search_stmt = $pdo->prepare("SELECT * FROM products WHERE product_name LIKE ? LIMIT ?, ?");
-    $search_stmt->execute(["%$search_q%", 0, 100]);
-    $search_data = $search_stmt->fetchAll(PDO::FETCH_OBJ);
+        $search_stmt = $pdo->prepare("SELECT * FROM products WHERE product_name LIKE ? LIMIT ?, ?");
+        $search_stmt->execute(["%$search_q%", 0, 100]);
+        $search_data = $search_stmt->fetchAll(PDO::FETCH_OBJ);
 
-    foreach($search_data as $sd){
+        echo "<div style='border-radius:9px;padding:12px'>";
+        foreach($search_data as $sd){
 ?>
-        <div style="border-bottom:1px solid #888;padding:12px 6px;background-color:#fff">
-            <a href ="/<?=$page?>/<?=$sd->product_url?>" style="color:#2b8eeb"><?=substr($sd->product_name, 0, 15)?></a>
-        </div>
+            <div style="border-bottom:1px solid #888;padding:12px 6px;background-color:#fff">
+                <a href ="/search/<?=$sd->product_url?>" style="color:#2b8eeb"><?=substr($sd->product_name, 0, 15)?></a>
+            </div>
 <?php 
        }
+       echo "</div>";
     }
 }
 
